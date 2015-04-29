e2151c6874e1e829da9e31bc93d751f85c0aa0b2
alex <alexzhou@linkedin.com>
2015-04-29 20:53:59 +0800

    map modified

--- front/js/main.js
+++ front/js/main.js
@@ -1,54 +1,3 @@
-zt = {
-    host : "http://127.0.0:8080/",
-    map : null,
-    whichMap : "",
-    mapContainer : {
-        width : 0,
-        height : 0,
-    },
-    worldMap : {
-    tooltip : {
-        trigger: 'item',
-        formatter: '{b}'
-    },
-    series : [
-        {
-            name: '世界地图',
-            type: 'map',
-            mapType: 'world',
-            roam: true,
-            selectedMode : 'single',
-            itemStyle:{
-                normal:{label:{show:false}},
-                emphasis:{label:{show:true}}
-            },
-            data:[],
-        }
-    ]
-    },
-    chinaMap : {
-        tooltip : {
-            trigger: 'item',
-            formatter: '{b}'
-        },
-        series : [
-            {
-                name: '中国',
-                type: 'map',
-                mapType: 'china',
-                selectedMode : 'multiple',
-                roam: true,
-                itemStyle:{
-                    normal:{label:{show:true}},
-                    emphasis:{label:{show:true}}
-                },
-                data:[
-                    {name:'广东',selected:true}
-                ]
-            }
-        ]
-    }
-}
 $(document).ready(function() {
     $('#fullpage').fullpage();
     init();
@@ -57,9 +6,18 @@ $(document).ready(function() {
 
 
 function init() {
-    zt.chart = echarts.init(document.getElementById('map'));
-    zt.chart.setOption(zt.chinaMap);
-    // changeToMap("chinaMap");
+    $("#map").css({
+        "width":"100%",
+        "height":parseInt(document.body.clientHeight)-37-231+"px"
+    })
+    zt.chart = echarts.init(document.getElementById('chart'));
+    zt.map = echarts.init(document.getElementById('map'));
+    zt.map.on("click",function(e){
+        console.log(e);
+    })
+    changeToMap("chinaMap");
+    zt.chart.setOption(zt.infoChart)
+    test();
 }
 
 
@@ -73,9 +31,9 @@ function getSiginNum() {
     });
 }
 
-
 function newPost() {
-    var url = zt.host +"/nP";
+    var url = zt.host +"/nM";
+    console.log(url);
     var data = {
         name :$("#msgName").val(),
         country :$("#msgLocation").val(),
@@ -91,11 +49,13 @@ function newPost() {
 }
 
 function newSignin() {
-    var url = zt.host="/nS";
+    var url = zt.host+"/nS";
+    console.log(url);
     var data = {
         //TODO
-        name :"",
+        name :$("#msgName").val(),
         location : 123,
+        context :$("#msgContext").val()
     }
     $.post(url,data,function(result){
         if ( result.success == false ) {
@@ -106,17 +66,15 @@ function newSignin() {
     });
 }
 
-
 function changeToMap(str) {
     if (zt.whichMap === str) {
         return true;
     }
     console.log(zt[str]);
-    zt.chart.setOption(zt[str]);
+    zt.map.setOption(zt[str]);
     zt.whichMap = str;
     return false;
 }
-
 
 function bindEvent() {
     $("#worldBut").on("click",function(){
@@ -135,4 +93,41 @@ function bindEvent() {
         $("#worldBut").removeClass("btnMA");
         return false;
     });
+    $("#sendMessage").on("click",function(){
+        console.log("hahaha");
+        newSignin();
+        newPost();
+    });
 }
+
+
+function test() {
+    var name = "sb";
+    var location = "中国";
+    var message = "今天好运气啊，老狼请吃鸡啊，你打电话我不接，你打他有啥用啊？？你打电话我不接";
+    for (var i = 0; i < 10; i++) {
+        appendMessage(name,location,i);
+        appendSignin(name,location,i);
+    }
+}
+
+function appendMessage(name,location,message) {
+    var str = zt.temp.message;
+    var $main = $("#message")
+    str = str.replace('{name}',name);
+    str = str.replace('{location}',location);
+    str = str.replace('{message}',message);
+    $main.append($(str));
+
+}
+
+function appendSignin(name,location,time) {
+    var str = zt.temp.signin;
+    var $main = $("#signinContainer");
+    str = str.replace('{name}',name);
+    str = str.replace('{location}',location);
+    str = str.replace('{time}',time);
+    $main.prepend($(str));
+}
+
+
